bc7a345ca7f19b257f8618fbb55642e0cbeb6ced
Idempotentiation <idempotentiation@protonmail.com>
2025-08-20 00:41:29 -0400

    Change starting skill update

--- data/sql/world/workflow/playercreateinfo_skills.sql
+++ data/sql/world/workflow/playercreateinfo_skills.sql
@@ -8,9 +8,10 @@ INSERT INTO `playercreateinfo_skills` (`raceMask`, `classMask`, `skill`, `rank`,
 (256, 0, 790, 0, 'Goblin - Racial'); -- To have their starting gear from cata, and the rogues have funky weapons.
 
 /*Updates existing starting skills to include worgen and goblins where relevant*/
-UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 46; -- Guns
-UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 173; -- Daggers
-UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256 WHERE `skill` = 160; -- 2h-mace goblins
+/*Only update skill if it has the default racemask (to prevent conflict with other modules that change starting weapon skills)*/
+UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 46 AND `racemask` = 36; -- Guns
+UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 173 AND `racemask` = 735; -- Daggers
+UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256 WHERE `skill` = 160 AND `racemask` = 1061; -- 2h-mace goblins
 /*Add appropriate faction language to worgen and goblins*/
 UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256 WHERE `skill` = 109; -- Orcish language goblins
 UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|2048 WHERE `skill` = 98; -- Common language worgen
