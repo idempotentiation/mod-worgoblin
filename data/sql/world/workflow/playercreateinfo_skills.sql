bafc812e58e3c4ee46ef28447bda15faa102a846
Idempotentiation <idempotentiation@protonmail.com>
2025-08-21 05:30:49 -0400

    Update bows skill

--- data/sql/world/workflow/playercreateinfo_skills.sql
+++ data/sql/world/workflow/playercreateinfo_skills.sql
@@ -9,6 +9,7 @@ INSERT INTO `playercreateinfo_skills` (`raceMask`, `classMask`, `skill`, `rank`,
 
 /*Updates existing starting skills to include worgen and goblins where relevant*/
 /*Only update skill if it has the default racemask (to prevent conflict with other modules that change starting weapon skills)*/
+UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 45 AND `racemask` = 650; -- Bows
 UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 46 AND `racemask` = 36; -- Guns
 UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256|2048 WHERE `skill` = 173 AND `racemask` = 735; -- Daggers
 UPDATE `playercreateinfo_skills` SET `racemask` = `racemask`|256 WHERE `skill` = 160 AND `racemask` = 1061; -- 2h-mace goblins
